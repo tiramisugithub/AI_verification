b3925f9eae1bd3d77b2a31bff6a9cbba7eefb259
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-08-25 15:35:58 +0900

    Initial commit

---
+++ src/main/java/com/sparta/aiverification/AiVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.aiverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiVerificationApplication.class, args);
+    }
+
+}
