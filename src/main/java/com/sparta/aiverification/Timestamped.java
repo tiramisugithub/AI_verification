a1ff8f5b174ad21b871715fedeafe35f4d940999
HeeJeongOh <ohhj1999@gmail.com>
2024-08-29 11:32:49 +0900

    Feat : Timestamped 자동을 위한 설정
    - @EnableJpaAuditing
    
    Issues: #5, #17

--- src/main/java/com/sparta/aiverification/Timestamped.java
+++ src/main/java/com/sparta/aiverification/Timestamped.java
@@ -2,6 +2,7 @@ package com.sparta.aiverification;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Setter
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -40,4 +42,10 @@ public abstract class Timestamped {
 
   @Column(updatable = false)
   private String deletedBy;
+
+  // 소프트 삭제 메서드
+  public void delete(String deletedByUser) {
+    this.deletedAt = LocalDateTime.now();
+    this.deletedBy = deletedByUser;
+  }
 }
