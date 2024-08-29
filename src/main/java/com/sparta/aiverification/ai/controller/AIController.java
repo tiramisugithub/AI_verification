139f0a6c0b4cdbcc78de8ae1595c6effd05b03b3
HeeJeongOh <ohhj1999@gmail.com>
2024-08-29 17:46:12 +0900

    Feat : AI CRUD 구성
    - AI 생성, 조회, 수정, 삭제 구성
    
    Issues: #20

--- src/main/java/com/sparta/aiverification/ai/controller/AIController.java
+++ src/main/java/com/sparta/aiverification/ai/controller/AIController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/regions")
+@RequestMapping("/ai")
 public class AIController {
 
   private  final AIService aiService;
