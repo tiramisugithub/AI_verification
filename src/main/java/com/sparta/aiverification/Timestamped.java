e0553fc862e9f8b05da70cd8aca0272ff32af6b7
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-08-29 23:21:52 +0900

    Fix : Conflict Merge
    Issues: #4

--- src/main/java/com/sparta/aiverification/Timestamped.java
+++ src/main/java/com/sparta/aiverification/Timestamped.java
@@ -1,6 +1,5 @@
-package com.sparta.aiverification.common;
+package com.sparta.aiverification;
 
-import com.sparta.aiverification.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
