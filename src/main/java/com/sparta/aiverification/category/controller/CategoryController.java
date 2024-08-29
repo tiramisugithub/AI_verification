187f9fa561eaec057eac9d91264c3bd369ed377f
HeeJeongOh <ohhj1999@gmail.com>
2024-08-29 15:43:48 +0900

    Feat : Category CRUD 구성
    - 카테고리 생성, 조회, 수정, 삭제
    
    Issues: #6

--- src/main/java/com/sparta/aiverification/category/controller/CategoryController.java
+++ src/main/java/com/sparta/aiverification/category/controller/CategoryController.java
@@ -1,5 +1,58 @@
 package com.sparta.aiverification.category.controller;
 
+import com.sparta.aiverification.category.service.CategoryService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
+
+  private final CategoryService categoryService;
+
+  @Autowired
+  public CategoryController(CategoryService categoryService) {
+    this.categoryService = categoryService;
+  }
+
+  // 1. 카테고리 생성
+  @PostMapping
+  public String createCategory(@RequestBody String categoryName) {
+    return categoryService.createCategory(categoryName);
+  }
+
+  // 2. 카테고리 목록 조회
+  @GetMapping
+  public List<String> getCategoryList() {
+    return categoryService.getCategoryList();
+  }
+
+  // 3. 카테고리 정보 조회
+  @GetMapping("/{categoryId}")
+  public String getCategory(@PathVariable Long categoryId) {
+    return categoryService.getCategory(categoryId);
+  }
+
+  // 4. 카테고리 수정
+  @PutMapping("/{categoryId}")
+  public String updateCategory(@PathVariable Long categoryId, @RequestBody String categoryName) {
+    return categoryService.updateCategory(categoryId, categoryName);
+  }
+
+  // 5. 카테고리 삭제
+  @DeleteMapping("/{categoryId}")
+  public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+    categoryService.deleteCategory(categoryId);
+    return ResponseEntity.ok("카테고리가 삭제되었습니다");
+  }
 
 }
--- src/main/java/com/sparta/aiverification/category/service/CategoryService.java
+++ src/main/java/com/sparta/aiverification/category/service/CategoryService.java
@@ -1,5 +1,54 @@
 package com.sparta.aiverification.category.service;
 
+import com.sparta.aiverification.category.entity.Category;
+import com.sparta.aiverification.category.repository.CategoryRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@AllArgsConstructor
+@Service
 public class CategoryService {
 
+  private final CategoryRepository categoryRepository;
+
+  @Transactional
+  public String createCategory(String categoryName) {
+    Category category = Category.builder().name(categoryName).build();
+    categoryRepository.save(category);
+    return category.getId().toString();
+  }
+
+  public List<String> getCategoryList() {
+    List<String> categoryList = new ArrayList<>();
+    List<Category> categoryResposneList = categoryRepository.findAll();
+    for(Category category : categoryResposneList){
+      categoryList.add(category.getName());
+    }
+    return categoryList;
+  }
+
+  public String getCategory(Long categoryId) {
+    Category category = categoryRepository.findById(categoryId).orElseThrow(NoSuchElementException::new);
+    return category.getName();
+  }
+
+  @Transactional
+  public String updateCategory(Long categoryId, String categoryName) {
+    Category category = categoryRepository.findById(categoryId).orElseThrow(NoSuchElementException::new);
+    category.update(categoryName);
+    return category.getId().toString();
+  }
+
+  @Transactional
+  public void deleteCategory(Long categoryId) {
+    Category category = categoryRepository.findById(categoryId).orElseThrow(NoSuchElementException::new);
+    // category.delete("username");
+  }
 }
