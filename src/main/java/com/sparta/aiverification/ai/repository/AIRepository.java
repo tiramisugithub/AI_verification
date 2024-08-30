1560c7bca65964baee15695e63952d5019b04e8e
HeeJeongOh <ohhj1999@gmail.com>
2024-08-30 16:21:31 +0900

    Feat : ai repository 수정
    findByMenu 삭제
    
    Issues: #43

--- src/main/java/com/sparta/aiverification/ai/repository/AIRepository.java
+++ src/main/java/com/sparta/aiverification/ai/repository/AIRepository.java
@@ -2,7 +2,6 @@ package com.sparta.aiverification.ai.repository;
 
 import com.sparta.aiverification.ai.entity.AI;
 import com.sparta.aiverification.user.entity.User;
-import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AIRepository extends JpaRepository<AI, Long> {
 
   Page<AI> findAllByUser(User user, Pageable pageable);
-  AI findByMenu(UUID menuId);
 
 }
